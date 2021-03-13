534a418ac341105551be36e176ddbe0222de4ee6
Anna Hanson <>
2021-03-12 17:30:04 -0700

    stub Boot/MVC project, link JPA dependency

---
+++ BootIRSCrud/src/main/java/com/skilldistillery/irsforms/BootIrsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.irsforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIrsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootIrsCrudApplication.class, args);
+	}
+
+}
---
+++ BootIRSCrud/src/test/java/com/skilldistillery/irsforms/BootIrsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.irsforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootIrsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

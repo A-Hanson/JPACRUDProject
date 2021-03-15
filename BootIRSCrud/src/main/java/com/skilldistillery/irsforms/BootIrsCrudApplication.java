feccc0bc05d4b0c302843f169abdaa5e7978803b
Anna Hanson <>
2021-03-15 09:17:03 -0600

    add dependencies for deploying

--- BootIRSCrud/src/main/java/com/skilldistillery/irsforms/BootIrsCrudApplication.java
+++ BootIRSCrud/src/main/java/com/skilldistillery/irsforms/BootIrsCrudApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.irsforms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.irsforms")
-public class BootIrsCrudApplication {
+public class BootIrsCrudApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootIrsCrudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootIrsCrudApplication.class, args);
